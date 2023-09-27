af5f894e1b2ecc08181a2db66054c144369bf0f5
Avv <it.spectre.ru@gmail.com>
2023-09-27 18:38:43 +0500

    add Logo on Navbar

--- dojo-helpdesk/app/components/Navbar.jsx
+++ dojo-helpdesk/app/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import Logo from './dojo-logo.png'
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt='Dojo helpdesk Logo'
+        width={70}
+        quality={100}
+        placeholder='blur'
+      />
       <h2>Dojo Helpdesk</h2>
       <Link href='/'>Dashboards</Link>
       <Link href='/tickets'>Tickets</Link>
